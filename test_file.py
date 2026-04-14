07a98b4601bffd89f34b5ce691a4fe3e0b5c30b5
demetramarlantis-netizen <demetramarlantis@gmail.com>
2026-04-14 10:09:12 -0500

    moved test file comments from readme to test_file

--- test_file.py
+++ test_file.py
@@ -1,1 +1,10 @@
-# we will put our test code here 
+# Test File (description):
+# To test the functionality of our program, the user should run the main Python scirpt. At the start of the game, the program should ask the user to enter thier name. Once the name has been entered and submitted, an directions screen will apear, explaining to the user how to play the game. There will be a start button that will begin the game. 
+#Once the user has pressed the start button, the game should begin. 
+#The screen should show 10 squares in teo rows of 5, each square with a diffrent color. The squares should remain visible for 1 second. 
+#After the first display disappears, the squares should reappear. Exactly one square should have changed color and the user should be able to click that one square. 
+#If the user then clicks the square that changed color, the program should display a message stating "correct." The score then should increase by 1. 
+#If the user then clocks the wrong the swuare, the program should display a message stating "incorrect." and the score should neither decrease nor increase. 
+#If the user gets to 5 points in the score, the program should end the game and display a message that the user has won the game! 
+#The sqaure that changes color should be selected randomly each round. 
+#Our program does not require any extra data so no test data file is needed. 
